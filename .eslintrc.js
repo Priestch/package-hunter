5825a308c83fc687a277eaae6c644d185faeb812
Priestch <priestch@gmail.com>
2018-11-30 23:26:29 +0800

    config eslint with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'extends': ['airbnb-base', 'plugin:prettier/recommended'],
+  'rules': {
+    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    "no-default-export": true,
+  }
+};
